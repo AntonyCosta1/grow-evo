efb47482be82189047eea8e4411de21c60fcbc56
Antony Costa <antonycosta210@gmail.com>
2026-03-05 12:54:49 -0300

    Ajuste vite config - Base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/grow-evo/',
+  base: '/',
   plugins: [
     tailwindcss(),
     react(),
